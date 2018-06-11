79c4afb276ea1fcbaf946df165f9db320447d3ff
dez <profacorp@gmail.com>
2018-06-11 19:07:04 +0300

    hot fixes

--- src/controlCenter/index.js
+++ src/controlCenter/index.js
@@ -5,21 +5,32 @@ let moduleConfig = {
     ],
     analyserUrl: 'http://localhost:3008'
 };
+let classes = {};
+
+const testProcess = (name, vector, analyserUrl) => {
+    return rp(`${analyserUrl}/test?vector=${vector}`)
+        .then(JSON.parse)
+        .then((matches) => {
+            if (Object.values(classes).includes(matches[0])) {
+                console.warn(`Suspicious process: ${name}`);
+            }
+        });
+};
 
 const makeDecision = (name, vector) => {
     const {knownMalwareList, analyserUrl} = moduleConfig;
     vector = JSON.stringify(vector.concat((new Array(100 - vector.length)).fill(0)));
     if (knownMalwareList.includes(name)) {
-        return rp(`${analyserUrl}/train?vector=${vector}&isMalware=1`)
-        .then(() => true);
+        if (!classes[name] && Object.keys(classes).length) {
+            classes[name] = Math.max(...Object.values(classes)) + 1;
+        } else if (!Object.keys(classes).length) {
+            classes[name] = 1;
+        }
+        return rp(`${analyserUrl}/train?vector=${vector}&malwareClass=${classes[name]}`)
+        .then(() => testProcess(name, vector, analyserUrl))
+        .catch(console.error);
     } else {
-        return rp(`${analyserUrl}/test?vector=${vector}`)
-        .then(JSON.parse)
-        .then((matches) => {
-            if (matches[0] > matches[1]) {
-                console.warn(`Suspicious process: ${name}`);
-            }
-        });
+        return testProcess(name, vector);
     }
 }
 
